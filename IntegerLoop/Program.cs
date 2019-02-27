98b7243f329e66d3c368df719bf0403d4e519c42
Puja Grover <puja_grover@yahoo.com>
2019-02-26 22:08:00 -0600

    Class5hwIntegerLoop finished. Commiting changes and pushing to my repository on github. Syncing

--- IntegerLoop/Program.cs
+++ IntegerLoop/Program.cs
@@ -8,6 +8,12 @@ namespace IntegerLoop
         {
             // TODO: Print even integers from 0 to 50.
 
+            //Console.WriteLine("Even integers from 0 to 50 are:");
+            
+            for (int i = 2; i < 51; i += 2)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
         }
     }
